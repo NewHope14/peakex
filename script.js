e1ece4629d4331c19af3b011d174ced2027f9a2d
NewHope14 <aasmamehta@msn.com>
2025-04-22 09:21:56 +0530

    bannerLeft

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿// Smooth Scroll Indicator - Optional
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollIcon = document.querySelector(".scroll-down");
+    scrollIcon.addEventListener("click", () => {
+        window.scrollTo({
+            top: document.querySelector(".highlights").offsetTop,
+            behavior: "smooth"
+        });
+    });
+});
+// Accordion
+const acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        panel.style.maxHeight
+            ? (panel.style.maxHeight = null)
+            : (panel.style.maxHeight = panel.scrollHeight + "px");
+    });
+}
